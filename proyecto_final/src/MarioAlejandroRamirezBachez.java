c924d1a0c77b544755406995b06033dcdcba2156
mariobachez <marioalejandrokiss98@gmail.com>
2020-05-17 18:19:35 -0600

    he agregado el codigo de la clase MarioAlejandroRamiorezBachez

--- proyecto_final/src/MarioAlejandroRamirezBachez.java
+++ proyecto_final/src/MarioAlejandroRamirezBachez.java
@@ -10,4 +10,53 @@
  */
 public class MarioAlejandroRamirezBachez {
     
+     private long NumCuenta;
+    private double CargoCuenta;
+    private double AbonoCuenta;
+    private double SaldoCuenta;
+    private char EstadoCuenta;
+
+    //set and get
+    public long getNumCuenta() {
+        return NumCuenta;
+    }
+
+    public void setNumCuenta(long numCuenta) {
+        this.NumCuenta = numCuenta;
+    }
+
+    public double getCargoCuenta() {
+        return CargoCuenta;
+    }
+
+    public void setCargoCuenta(double cargoCuenta) {
+        this.CargoCuenta = cargoCuenta;
+    }
+
+    public double getAbonoCuenta() {
+        return AbonoCuenta;
+    }
+
+    public void setAbonoCuenta(double abonoCuenta) {
+        this.AbonoCuenta = abonoCuenta;
+    }
+
+    public double getSaldoCuenta() {
+        return SaldoCuenta;
+    }
+//aca ed donde modificamos
+    public void setSaldoCuenta() {//se elimina  el atributo double 
+        //se escrive la formula siguiente para calcular saldo
+        this.SaldoCuenta = SaldoCuenta + this.AbonoCuenta - this.CargoCuenta;
+    }
+
+    public char getEstadoCuenta() {
+        return EstadoCuenta;
+    }
+
+    public void setEstadoCuenta(char estadoCuenta) {
+        this.EstadoCuenta = estadoCuenta;
+    }
 }
+
+
