4bf216bd45af6c5791453b802171bdb4235d6460
mariobachez <marioalejandrokiss98@gmail.com>
2020-05-17 18:00:06 -0600

    he agregadoel codigo la clase convertir tipo dato

--- proyecto_final/src/ConvertirTipoDato.java
+++ proyecto_final/src/ConvertirTipoDato.java
@@ -10,4 +10,26 @@
  */
 public class ConvertirTipoDato {
     
+    public static void main(String[] args) {
+        ConvertirTipoDato polimorfismo  = new ConvertirTipoDato ();//obgeto de la clase
+        System.out.println("Valor int a String " + polimorfismo.convertirAString(10));
+        System.out.println("Valor double a String " + polimorfismo.convertirAString(10.0));
+        System.out.println("Valor a String : " + polimorfismo.convertirAString(10.12));
+        
+        }
+        public String convertirAString(int valorInt){//convertir int a string
+        String conversion = String.valueOf(valorInt);
+        return conversion;
+       }
+        
+        public String convertirAString(double valorDouble){//converir double a String
+        String conversion = String.valueOf(valorDouble);
+        return conversion;
+        }
+        
+        public String convertirAString(float valorFloat){//comvertir float a String
+        String conversion = String.valueOf(valorFloat);
+        return conversion;
+        }
 }
+
