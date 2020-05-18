e65a7a3c150e42cbe396fbedce5984fce25ccdd1
Nahum503 <nahumpineda1999@gmail.com>
2020-05-17 18:26:27 -0600

    eh agregado codigo a la clase con mi nombre

--- proyecto_final/src/NahumIsraelPinedaPaniagua.java
+++ proyecto_final/src/NahumIsraelPinedaPaniagua.java
@@ -9,5 +9,32 @@
  * @author ADMIN
  */
 public class NahumIsraelPinedaPaniagua {
+     long NumDui;
+    String NombreCliente;
+    String TelCliente;
+
+    public long getNumDui() {
+        return NumDui;
+    }
+
+    public void setNumDui(long numDui) {
+        this.NumDui = numDui;
+    }
+
+    public String getNombreCliente() {
+        return NombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.NombreCliente = nombreCliente;
+    }
+
+    public String getTelCliente() {
+        return TelCliente;
+    }
+
+    public void setTelCliente(String telCliente) {
+        this.TelCliente = telCliente;
+    }
     
 }
